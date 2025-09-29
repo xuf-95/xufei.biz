886588bc2ed1b8d23cbc8ea49c7ca0cb573ed859
sherlock.xpf <Sherlock.xpf@gmail.com>
2025-09-29 21:15:17 +0800

    homepage

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -24,6 +24,47 @@ export default ((opts?: Options) => {
             </li>
           ))}
         </ul>
+        <div class="badges">
+          <a href="https://www.wikipedia.org/" target="_blank" rel="noopener noreferrer">
+            <img src="https://lmnt.me/files/images/badges/wikipedia.gif" alt="Wikipedia" />
+          </a>
+          <a href="https://archive.org/" target="_blank" rel="noopener noreferrer">
+            <img src="https://lmnt.me/files/images/badges/internet-archive.gif" alt="Internet Archive" />
+          </a>
+          <a href="https://www.apache.org/" target="_blank" rel="noopener noreferrer">
+            <img src="https://www.apache.org/foundation/press/kit/poweredBy/Apache_PoweredBy.svg" alt="Apache" width="88" height="31" />
+          </a>
+
+          <a href="https://flink.apache.org/" target="_blank" rel="noopener noreferrer">
+            <img src="https://www.apache.org/logos/res/flink/default.png" alt="Flink" width="88" height="31" />
+          </a>
+
+          <a href="https://hadoop.apache.org/" target="_blank" rel="noopener noreferrer">
+            <img src="https://www.apache.org/logos/res/hadoop/default.png" alt="Hadoop" width="88" height="31" />
+          </a>
+
+          <a href="https://hive.apache.org/" target="_blank" rel="noopener noreferrer">
+            <img src="https://www.apache.org/logos/originals/hive.svg" alt="Hive" width="88" height="31" />
+          </a>
+
+          <a href="https://hbase.apache.org/" target="_blank" rel="noopener noreferrer">
+            <img src="https://www.apache.org/logos/res/hbase/default.png" alt="HBase" width="88" height="31" />
+          </a>
+
+          <a href="https://spark.apache.org/" target="_blank" rel="noopener noreferrer">
+            <img src="https://www.apache.org/logos/res/spark/default.png" alt="Spark" width="88" height="31" />
+          </a>
+
+          <a href="https://kafka.apache.org/" target="_blank" rel="noopener noreferrer">
+            <img src="https://www.apache.org/logos/res/kafka/default.png" alt="Kafka" width="88" height="31" />
+          </a>
+
+          <a href="https://zookeeper.apache.org/" target="_blank" rel="noopener noreferrer">
+            <img src="https://www.apache.org/logos/res/zookeeper/default.png" alt="Zookeeper" width="88" height="31" />
+          </a>
+
+        </div>
+
       </footer>
     )
   }
